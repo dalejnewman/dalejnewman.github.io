5b3c0bf215d65b0ba94f28eaadfb09b6eea303a9
Dale Newman <41090495+dalejnewman@users.noreply.github.com>
2020-04-07 08:48:02 +1000

    Update tabletop.js

--- tabletop-master/tabletop-master/src/tabletop.js
+++ tabletop-master/tabletop-master/src/tabletop.js
@@ -1,6 +1,6 @@
 <script src='https://cdnjs.cloudflare.com/ajax/libs/tabletop.js/1.5.1/tabletop.min.js'></script>
 <script type='text/javascript'>    
-  var publicSpreadsheetUrl = 'https://docs.google.com/spreadsheets/d/1sbyMINQHPsJctjAtMW0lCfLrcpMqoGMOJj6AN-sNQrc/pubhtml';
+  var publicSpreadsheetUrl = 'https://docs.google.com/spreadsheets/d/1oANUf10vKh8iL8c1v6jcEctIMt7_bHqDfcq2OE3yyew/pubhtml';
 
   function init() {
     Tabletop.init( { key: publicSpreadsheetUrl,
