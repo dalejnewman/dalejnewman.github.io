f7a6bf8660905a0d43f283693cb966f7d99f7170
Dale Newman <41090495+dalejnewman@users.noreply.github.com>
2020-04-20 10:48:26 +1000

    Update main.js

--- main.js
+++ main.js
@@ -3,11 +3,9 @@
   var publicSpreadsheetUrl = 'https://docs.google.com/spreadsheets/d/1oANUf10vKh8iL8c1v6jcEctIMt7_bHqDfcq2OE3yyew/edit?usp=sharing';
 
   function init() {
-    Tabletop.init( {
-      key: publicSpreadsheetUrl,
+    Tabletop.init( { key: publicSpreadsheetUrl,
       callback: showInfo,
-      simpleSheet: true 
-    } )
+      simpleSheet: true } )
   }
 
   function showInfo(data, tabletop) {
