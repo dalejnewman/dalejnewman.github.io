0d1e10c06f0a35ac48c9adee4e3cf2b1543503c9
Dale Newman <41090495+dalejnewman@users.noreply.github.com>
2020-04-20 11:00:19 +1000

    Changed all Script
    Used another main.js script
    
    var publicSpreadsheetUrl = 'https://docs.google.com/spreadsheets/d/1DpRZ4IA6Xd_rd4sNnkpuTkp_-6KOEJJWS-VqZxw4oU0/edit?usp=sharing';
    
      function init() {
        Tabletop.init( { key: publicSpreadsheetUrl,
          callback: showInfo,
          simpleSheet: true } )
      }
    
      function showInfo(data, tabletop) {
        alert('Successfully processed!')
        console.log(data);
      }
    
      window.addEventListener('DOMContentLoaded', init)

--- main.js
+++ main.js
@@ -1,15 +1,14 @@
-  var publicSpreadsheetUrl = 'https://docs.google.com/spreadsheets/d/1DpRZ4IA6Xd_rd4sNnkpuTkp_-6KOEJJWS-VqZxw4oU0/edit?usp=sharing';
+function init() {
+  Tabletop.init( {
+    key: 'https://docs.google.com/spreadsheets/d/1DpRZ4IA6Xd_rd4sNnkpuTkp_-6KOEJJWS-VqZxw4oU0/pubhtml',
+    simpleSheet: true }
+  ).then(function(data, tabletop) { 
+    console.log(data)
+  })
+}
+window.addEventListener('DOMContentLoaded', init) 
 
-  function init() {
-    Tabletop.init( { key: publicSpreadsheetUrl,
-      callback: showInfo,
-      simpleSheet: true } )
-  }
 
-  function showInfo(data, tabletop) {
-    alert('Successfully processed!')
-    console.log(data);
-  }
 
-  window.addEventListener('DOMContentLoaded', init)
+
 
