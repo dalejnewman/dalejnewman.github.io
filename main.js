bbefc27e874a0e63fa0d82901a415f9c7c0bcde5
Dale Newman <41090495+dalejnewman@users.noreply.github.com>
2020-04-20 11:06:52 +1000

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 function init() {
   Tabletop.init( {
-    key: 'https://docs.google.com/spreadsheets/d/2PACX-1vQha3Sn6AAsnZsQwwqmSTgGxbZe4pEl-lGcdxjrGXsVgOFfekIXjNQcXsoJtYlWabXUVA6fhE8n8Zdf/pubhtml',
+    key: 'https://docs.google.com/spreadsheets/d/1oANUf10vKh8iL8c1v6jcEctIMt7_bHqDfcq2OE3yyew/edit?usp=sharing',
     simpleSheet: true }
   ).then(function(data, tabletop) { 
     console.log(data)
