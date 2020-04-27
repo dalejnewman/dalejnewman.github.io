cc9e8397d01f6a0ad941fca60ee5fc33bc4770b3
Dale Newman <41090495+dalejnewman@users.noreply.github.com>
2020-04-27 11:07:28 +1000

    Update background.js

--- background.js
+++ background.js
@@ -1,1 +1,22 @@
-https://docs.google.com/spreadsheets/d/1UK3tC3_Wn5ufVYccTNOY1kj5AREe4NPwMpWH3qY9APQ/edit?usp=sharing
+var publicSpreadsheetUrl = 'https://docs.google.com/spreadsheets/d/1UK3tC3_Wn5ufVYccTNOY1kj5AREe4NPwMpWH3qY9APQ/edit?usp=sharing';
+
+  function init() {
+    Tabletop.init( {
+      key: publicSpreadsheetUrl,
+      callback: showInfo,
+      simpleSheet: true 
+    } )
+  }
+
+  function showInfo(data, tabletop) {
+    data.forEach(function(data) {
+    header.innerHTML = data.header;
+    header2.innerHTML = data.header2;
+    body.innerHTML = data.body;
+    body2.innerHTML = data.body2;
+    body3.innerHTML = data.body3;
+    
+ });
+}
+
+  window.addEventListener('DOMContentLoaded', init)
